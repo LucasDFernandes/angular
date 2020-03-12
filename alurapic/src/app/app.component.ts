59f9554fb63bd8798ea04fc58409f86333b8c910
Lucas Fernandes <lucas.rj.fernandes@gmail.com>
2020-03-12 17:38:09 -0300

    alurapic

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  
+  photos: any[] = []; 
+  
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    
+      this.photoService
+        .listFromUser('flavio')
+        .subscribe(photos => this.photos = photos);
+  }
+}
